39b040a3a948b8ac76f5a100ed0a7ef37bf3ceb1
David Chambers <dlchambersjr@gmail.com>
2018-11-03 16:44:28 -0700

    Merge new class repo info

---
+++ labs/lab-14/src/models/books-model.js
@@ -0,0 +1,16 @@
+import mongoose, { Schema } from 'mongoose';
+
+// Book Schema
+const bookSchema = new Schema({
+  author: { type: Schema.Types.ObjectId, ref: 'Author' },
+  title: String,
+  genre: String,
+});
+
+
+//Schema methods
+
+// Books Model
+const Book = mongoose.model('Books', bookSchema);
+
+export default Book;
